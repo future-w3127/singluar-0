c110bde3b0c464b1ae01b348e544a3343e480f0c
hunzsig <mzyhaohaoren@qq.com>
2021-09-15 20:57:18 +0800

    chore(*): *

--- main.lua
+++ main.lua
@@ -1,66 +1,9 @@
--- The game starts here 游戏从 __SINGLUAR__ 函数开始运行
-function __SINGLUAR__()
+require("tpl.ability") -- 技能模版
+require("tpl.item") -- 物品模版
+require("tpl.unit") -- 单位模版
 
-    require("scripts.setup") -- setup 游戏准备
-    require("tpl.ability") -- 技能模版
-    require("tpl.item") -- 物品模版
-    require("tpl.unit") -- 单位模版
+--- 流程
+require("process.start")
+require("process.test")
 
-    local u1 = TPL_UNIT.U1.create(Player(1), 0, 0, 66.6).level(1)
-    u1.attr()
-      .reborn(0.5)
-      .hp(5000).hpRegen(5)
-      .mp(100)--.mpRegen(-1)
-      .move(522)
-      .attack(91).attackRange(1000).attackSpeed(300)
-      .attackRipple(30)
-      .missileAdd({
-        priority = 1,
-        model = "DragonHawkMissile",
-        homing = true, height = 300, speed = 700, shake = 'rand',
-        scatter = 10,
-    })
-      .crit(10).odds("crit", 10)
-      .hpSuck("+=10")
-      .mpSuck("+=10")
-      .enchantWeapon("grass", "+=1")
-      .enchantWeapon("poison", "+=1")
-      .punish(2000)
-      .weight("+=10")
-
-    u1.onDamage(function(evtData)
-        u1.exp("+=10")
-    end)
-
-    --time.setInterval(3, function()
-    --    u1.attr().attack("+=10;2")
-    --end)
-    --time.setInterval(3, function()
-    --    Player(1).alert(string.random(10))
-    --end)
-
-    for _ = 1, 5 do
-        local u2 = Player(2).unit(TPL_UNIT.CenariusNightmare, -400, 400, 66.6).period(1000)
-        u2.attr()
-          .primary("agi")
-          .move(50)
-          .hp(1000000)
-          .mpRegen("+=10")
-          .attack(109).attackRange(300)
-          .punish(1000)
-          .avoid(35)
-          .lightningAdd({ lightingType = LIGHTING_TYPE.thunderRed, focus = 2 })
-    end
-
-    u1.abilityPush(TPL_ABILITY.AB1)
-    u1.abilityPush(TPL_ABILITY.AB2)
-    u1.abilityPush(TPL_ABILITY.AB3, 4)
-
-    time.setInterval(5, function()
-        u1.ability()[1].castRadiusAdv(math.rand(50, 1200), nil)
-    end)
-
-    u1.itemPush(TPL_ITEM.IT1, 2) -- 物品A
-    u1.itemPush(TPL_ITEM.IT2, 3) -- 物品B
-
-end
+Process("start").start()
---
+++ process/test.lua
@@ -0,0 +1,69 @@
+local process = Process("test")
+
+process.onSetup(function(this)
+
+    local u1 = TPL_UNIT.U1.create(Player(1), 0, 0, 66.6).level(1)
+    u1.attr()
+      .reborn(0.5)
+      .hp(1000).hpRegen(5)
+      .mp(100)--.mpRegen(-1)
+      .move(522)
+      .attack(91).attackRange(1000).attackSpeed(100)
+      .attackRipple(30)
+      .missileAdd({
+        priority = 1,
+        model = "DragonHawkMissile",
+        homing = true, height = 300, speed = 700, shake = 'rand',
+        scatter = 100, radius = 10000,
+    })
+      .crit(10).odds("crit", 10)
+      .hpSuck("+=10")
+      .mpSuck("+=10")
+      .enchantWeapon("grass", "+=1")
+      .enchantWeapon("poison", "+=1")
+      .punish(2000)
+      .weight("+=10")
+
+    u1.onDamage(function(evtData)
+        u1.exp("+=10")
+    end)
+    this.stage("u1", u1)
+
+    --time.setInterval(3, function()
+    --    u1.attr().attack("+=10;2")
+    --end)
+    --time.setInterval(3, function()
+    --    Player(1).alert(string.random(10))
+    --end)
+
+    local u2s = {}
+    for _ = 1, 7 do
+        local u2 = Player(2).unit(TPL_UNIT.CenariusNightmare, -400, 400, 66.6).period(1000)
+        u2.attr()
+          .primary("agi")
+          .move(50)
+          .hp(1000000)
+          .mpRegen("+=10")
+          .attack(109).attackRange(300)
+          .punish(1000)
+          .avoid(35)
+          .lightningAdd({ lightingType = LIGHTING_TYPE.thunderRed, focus = 2 })
+        table.insert(u2s, u2)
+    end
+    this.stage("u2s", u2s)
+
+    u1.abilityPush(TPL_ABILITY.AB1)
+    u1.abilityPush(TPL_ABILITY.AB2)
+    u1.abilityPush(TPL_ABILITY.AB3, 6)
+
+    u1.itemPush(TPL_ITEM.IT1, 2) -- 物品A
+    u1.itemPush(TPL_ITEM.IT2, 3) -- 物品B
+
+end)
+
+process.onDestroy(function(this)
+    this.stage("u1").destroy()
+    for _, u in ipairs(this.stage("u2s")) do
+        u.destroy()
+    end
+end)
