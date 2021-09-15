7cf8f1cf20fe4970b20c983978caad1b19a2929b
hunzsig <mzyhaohaoren@qq.com>
2021-09-15 22:05:50 +0800

    chore(*): *

--- process/start.lua
+++ process/start.lua
@@ -1,58 +1,73 @@
 local process = Process("start")
 
 process.onSetup(function(this)
-    -- 调试自动去除迷雾
-    Game().fog(not DEBUGGING).mark(not DEBUGGING)
 
-    -- 资源转化
-    Game().convert("gold", "lumber", 1000000) -- 1木 = 1000000金
-    Game().convert("silver", "gold", 100) -- 1金 = 100银
-    Game().convert("copper", "silver", 100) -- 1银 = 100铜
+    if (this.stage("init") == nil) then
+        -- 资源转化
+        Game().convert("gold", "lumber", 1000000) -- 1木 = 1000000金
+        Game().convert("silver", "gold", 100) -- 1金 = 100银
+        Game().convert("copper", "silver", 100) -- 1银 = 100铜
 
-    -- 命令
-    --- -gg 投降
-    Game().command("^-gg$", function()
-        evtData.triggerPlayer.quit("GG")
-    end)
-    --- -apm 查看玩家分钟操作数
-    Game().command("^-apm$", function(evtData)
-        echo("您的apm为:" .. evtData.triggerPlayer.apm(), evtData.triggerPlayer.__HANDLE__)
-    end)
-    --- -d [+|-|=][NUMBER]减少/增加/设置视距
-    Game().command("^-d [-+=]%d+$", function(evtData)
-        local cds = string.explode(" ", string.lower(evtData.chatString))
-        local first = string.sub(cds[2], 1, 1)
-        if (first == "+" or first == "-" or first == "=") then
-            --视距
-            local v = string.sub(cds[2], 2, string.len(cds[2]))
-            v = math.abs(tonumber(v))
-            if (v > 0) then
-                local val = math.abs(v)
-                if (first == "+") then
-                    evtData.triggerPlayer.cameraDistance("+=" .. val)
-                elseif (first == "-") then
-                    evtData.triggerPlayer.cameraDistance("-=" .. val)
-                elseif (first == "=") then
-                    evtData.triggerPlayer.cameraDistance(val)
+        -- 命令
+        --- -gg 投降
+        Game().command("^-gg$", function()
+            evtData.triggerPlayer.quit("GG")
+        end)
+        --- -apm 查看玩家分钟操作数
+        Game().command("^-apm$", function(evtData)
+            echo("您的apm为:" .. evtData.triggerPlayer.apm(), evtData.triggerPlayer.__HANDLE__)
+        end)
+        --- -d [+|-|=][NUMBER]减少/增加/设置视距
+        Game().command("^-d [-+=]%d+$", function(evtData)
+            local cds = string.explode(" ", string.lower(evtData.chatString))
+            local first = string.sub(cds[2], 1, 1)
+            if (first == "+" or first == "-" or first == "=") then
+                --视距
+                local v = string.sub(cds[2], 2, string.len(cds[2]))
+                v = math.abs(tonumber(v))
+                if (v > 0) then
+                    local val = math.abs(v)
+                    if (first == "+") then
+                        evtData.triggerPlayer.cameraDistance("+=" .. val)
+                    elseif (first == "-") then
+                        evtData.triggerPlayer.cameraDistance("-=" .. val)
+                    elseif (first == "=") then
+                        evtData.triggerPlayer.cameraDistance(val)
+                    end
                 end
             end
+        end)
+        if (DEBUGGING) then
+            --- -apm 查看玩家分钟操作数
+            Game().command("^-proc [a-zA-Z0-9_]+$", function(evtData)
+                local p = string.trim(evtData.matchedString)
+                p = string.sub(p, 7, string.len(p))
+                local proc = Processes.get(p)
+                if (instanceof(proc, "Process")) then
+                    print_mb(p .. "流程已重置")
+                    proc.start()
+                end
+            end)
         end
-    end)
 
-    --- 任务
-    Quest("主动投降", "right")
-        .icon("ReplaceableTextures\\CommandButtons\\BTNTomeOfRetraining.blp")
-        .description("-gg 投降并退出")
-    Quest("查看你的APM数值", "right")
-        .icon("ReplaceableTextures\\CommandButtons\\BTNTomeOfRetraining.blp")
-        .description("-apm 查看你的APM数值")
-    Quest("调整你的视距", "right")
-        .icon("ReplaceableTextures\\CommandButtons\\BTNTomeOfRetraining.blp")
-        .description({
-        "-d +[number] 增加视距",
-        "-d -[number] 减少视距",
-        "-d =[number] 设置视距",
-    })
+        --- 任务
+        Quest("主动投降", "right")
+            .icon("ReplaceableTextures\\CommandButtons\\BTNTomeOfRetraining.blp")
+            .description("-gg 投降并退出")
+        Quest("查看你的APM数值", "right")
+            .icon("ReplaceableTextures\\CommandButtons\\BTNTomeOfRetraining.blp")
+            .description("-apm 查看你的APM数值")
+        Quest("调整你的视距", "right")
+            .icon("ReplaceableTextures\\CommandButtons\\BTNTomeOfRetraining.blp")
+            .description({
+            "-d +[number] 增加视距",
+            "-d -[number] 减少视距",
+            "-d =[number] 设置视距",
+        })
+    end
+
+    -- 调试自动去除迷雾
+    Game().fog(not DEBUGGING).mark(not DEBUGGING)
 
     --- 事件反应
     local function _z(z, offset)
@@ -165,7 +180,7 @@ process.onSetup(function(this)
             x = evtData.triggerUnit.x(),
             y = evtData.triggerUnit.y(),
             z = _z(evtData.triggerUnit.z(), 0),
-            height = 150,
+            height = 1150,
             duration = 0.5,
         })
     end)
@@ -182,6 +197,7 @@ process.onSetup(function(this)
         })
     end)
 
+    this.stage("init", true)
     this.next("test")
 
 end)
