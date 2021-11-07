e6d58f3e75e63369205992993ecf3fc03f57a83f
hunzsig <mzyhaohaoren@qq.com>
2021-11-07 13:21:21 +0800

    chore(*): *

---
+++ scripts/process/dialog.lua
@@ -0,0 +1,27 @@
+local process = Process("dialog")
+
+process.onSetup(function(this)
+
+    Dialog(
+        "选择难度",
+        {
+            { hotkey = "Q", label = "一般困难", value = 1 },
+            { hotkey = "W", label = "非常困难", value = 2 },
+            { hotkey = "E", label = "极奇困难", value = 3 },
+            { hotkey = "R", label = "破天荒难", value = 4 },
+        },
+        function(btnData)
+            if (btnData.hotkey == "Q") then
+                --搞点事情
+            elseif (btnData.hotkey == "W") then
+                --搞点事情
+            elseif (btnData.hotkey == "E") then
+                --搞点事情
+            else
+                --搞点事情
+            end
+            echo("选择了：" .. btnData.label .. "等级：" .. btnData.value)
+        end
+    ).showFirst()
+
+end)
---
+++ scripts/process/team.lua
@@ -0,0 +1,32 @@
+local process = Process("team")
+
+process.onSetup(function(this)
+
+    local t1 = Team("小小吃饭队")
+        .nameSync(true)
+        .colorSync(true)
+        .members({ 2, 3, 4, 5, 6 })
+        .color(PLAYER_COLOR_BLUE)
+
+    this.stage("t1", t1)
+
+    time.setTimeout(3, function()
+        t1.name("小小吐饭队").color(PLAYER_COLOR_GREEN)
+    end)
+
+    local us = {}
+    for _ = 1, 20 do
+        local u = t1.unit(TPL_UNIT.CenariusNightmare, 0, 0)
+        table.insert(us, u)
+    end
+    this.stage("us", us)
+
+end)
+
+process.onDestroy(function(this)
+    this.stage("t1").destroy()
+    for _, u in ipairs(this.stage("us")) do
+        u.destroy()
+    end
+end)
+
