251f26ec21e26a6564c905dcde1e90474869945c
hunzsig <mzyhaohaoren@qq.com>
2021-11-13 22:10:28 +0800

    chore(*): *

--- scripts/process/dialog.lua
+++ scripts/process/dialog.lua
@@ -10,17 +10,18 @@ process.onSetup(function(this)
             { hotkey = "E", label = "极奇困难", value = 3 },
             { hotkey = "R", label = "破天荒难", value = 4 },
         },
-        function(btnData)
-            if (btnData.hotkey == "Q") then
+        function(evtData)
+            evtData.triggerDialog.destroy()
+            if (evtData.hotkey == "Q") then
                 --搞点事情
-            elseif (btnData.hotkey == "W") then
+            elseif (evtData.hotkey == "W") then
                 --搞点事情
-            elseif (btnData.hotkey == "E") then
+            elseif (evtData.hotkey == "E") then
                 --搞点事情
             else
                 --搞点事情
             end
-            echo("选择了：" .. btnData.label .. "等级：" .. btnData.value)
+            echo("选择了：" .. evtData.label .. "等级：" .. evtData.value)
         end
     ).show()
 
