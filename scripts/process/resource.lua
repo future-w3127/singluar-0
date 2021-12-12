eb87848e1423096cdddafb16d8809f9e99beaf30
hunzsig <mzyhaohaoren@qq.com>
2021-12-12 17:34:24 +0800

    chore(*): *

--- scripts/process/resource.lua
+++ scripts/process/resource.lua
@@ -12,9 +12,25 @@ process.onSetup(function(this)
         Player(1).copper("+=166")
     end)
 
+    print_mb("对比测试")
     print('Compare1 ', Game().resourceCompare({ silver = 1, gold = 1 }, { silver = 98, copper = 22 }))
     print('Compare2 ', Game().resourceCompare({ silver = 10, copper = 1000 }, { copper = 2000 }))
     print('Compare3 ', Game().resourceCompare({ gold = 2 }, { copper = 1001, silver = 190 }))
+
+    print_mb("上->下转换")
+    print_r(Game().resourceU2L({ silver = 44, gold = 1 }))
+    print_r(Game().resourceU2L({ gold = 1, silver = 2, copper = 3 }))
+    print_r(Game().resourceU2L({ gold = 2, copper = 1234 }))
+
+    print_mb("下->上转换")
+    print_r(Game().resourceL2U({ copper = 16400 }))
+    print_r(Game().resourceL2U({ copper = 11203 }))
+    print_r(Game().resourceL2U({ copper = 21374 }))
+
+    print_mb("比例计算")
+    print_r(Game().resourceCale({ gold = 1, silver = 2, copper = 3 }, 0.5))
+    print_r(Game().resourceCale({ gold = 2, silver = 1, copper = 7 }, 0.3))
+    print_r(Game().resourceCale({ gold = 7, silver = 13, copper = 1111 }, 0.5))
 
 end)
 
